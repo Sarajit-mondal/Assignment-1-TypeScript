cd0ef5cad47afdd7ea475f1fbb3282af17b1f8a2
sarajit <bdsarajit499@gamil.com>
2025-05-08 00:14:01 +0600

    problem 2 completed

--- tsProblem.ts
+++ tsProblem.ts
@@ -7,6 +7,28 @@ if(toUpper === true || toUpper === undefined){
 }
 
 
+type Book =
+    {
+        title : string;
+        rating : number;
+    }
+type Books = Book[] 
+
+
+const books :  Books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+    { title: "Book D", rating: 3.0 }
+  ];
+
+function filterByRating(items: Books){
+    console.log( items.filter(rat => rat.rating >= 4))
+}
+
+  
+  filterByRating(books); 
+  // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
 
 
 
